175bb585d91ac6e217bd102552fd3ee3b8c4f701
ImogeneKaufman <imogene.kaufman@gmail.com>
2015-07-02 16:43:04 +0000

    sdgdhadhd

---
+++ models/model.rb
@@ -0,0 +1,14 @@
+class Organization
+  
+  attr_accessor :name, :link, 
+  
+  def initialize(name, link,)
+    @name = name
+    @link = link
+  end
+ 
+end
+    
+  
+
+# different types of causes: animals and animal rights, medical research, child rights, civil rights, environment, homelessness, hunger, literacy, mental health, women's rights
