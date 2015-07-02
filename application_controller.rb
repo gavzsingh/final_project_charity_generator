452ea430a90067fd05052cec0211db9c800cfe50
ImogeneKaufman <imogene.kaufman@gmail.com>
2015-07-02 14:13:22 +0000

    sdfsd

--- application_controller.rb
+++ application_controller.rb
@@ -4,5 +4,14 @@ Bundler.require
 require './models/model.rb'
 
 class MyApp < Sinatra::Base
+ 
+  get '/' do
+    erb :index
+  end
+
+  post'results' do
+    puts params
+    erb :index
+  end
   
 end
